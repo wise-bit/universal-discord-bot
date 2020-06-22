f5145d9e75275a9b7c7e326e06275ba37cd9a084
Satrajit Chatterjee <satrajit2016@gmail.com>
2020-06-22 00:52:04 -0400

    added timed diary message feature

--- bot.js
+++ bot.js
@@ -1,95 +1,129 @@
-var Discord = require('discord.io');
-var logger = require('winston');
+var Discord = require("discord.io");
+var logger = require("winston");
 // var auth = require('./auth.json'); // for offline access
-let price = require('crypto-price')
+let price = require("crypto-price");
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
+logger.add(new logger.transports.Console(), {
+  colorize: true,
 });
-logger.level = 'debug';
+logger.level = "debug";
+
+// Functional variables
+const defaultTimerInterval = 3600;
+var timerInterval;
+var intervalId;
+
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: process.env.BOT_TOKEN,
-   autorun: true
+  token: process.env.BOT_TOKEN,
+  autorun: true,
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+
+bot.on("ready", function (evt) {
+  logger.info("Connected");
+  logger.info("Logged in as: ");
+  logger.info(bot.username + " - (" + bot.id + ")");
+  startInterval(defaultTimerInterval);
 });
 
 var currChannelID = null; // this changes as soon as it is called from another channel
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-    currChannelID = channelID;
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        // console.log(args);
+bot.on("message", function (user, userID, channelID, message, evt) {
+  currChannelID = channelID;
+  // Our bot needs to know if it will execute a command
+  // It will listen for messages that will start with `!`
+  if (message.substring(0, 1) == "!") {
+    var args = message.substring(1).split(" ");
+    var cmd = args[0];
+
+    // console.log(args);
+    args = args.splice(1);
+
+    switch (cmd) {
+      // !ping
+      case "play":
+        messageFunction("reevaluating purpose in life");
+        break;
+      case "help":
+        bot.sendMessage({
+          to: channelID,
+          message:
+            "Roses are red, violets are blue\nI'm dysfunctional, and so are you",
+        });
+        break;
+      // Just add any case commands if you want to..
+      case "testInput": // start splicing members until you run out, where you send error
+        if (args.length < 2)
+          messageFunction("You must tag exactly two users to start game...");
+        else
+          messageFunction(
+            "New game between " + args[0] + " and " + args[1] + " started!"
+          );
+        args = args.splice(2);
+        // shed spaces from both sides
+        // check if both @s are valid and instantiate game
+        break;
+
+      case "crypto": // start splicing members until you run out, where you send error
+        if (args.length < 2) messageFunction("Format: !crypto base crypto");
+        else cryptoPrice(args[0], args[1]);
+        args = args.splice(2);
+        break;
+
+      case "changetimer":
+        if (args.length < 1)
+          messageFunction("Format: !changetimer interval(in seconds");
+        if (!isNaN(args[0])) {
+          timerInterval = parseFloat(args[0]) * 1000;
+          clearInterval(intervalId);
+          startInterval(timerInterval);
+        }
         args = args.splice(1);
+        break;
 
-        switch(cmd) {
-            // !ping
-            case 'play':
-                messageFunction('reevaluating purpose in life');
-            break;
-            case 'help':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Roses are red, violets are blue\n\I\'m dysfunctional, and so are you'
-                });
-            break;
-            // Just add any case commands if you want to..
-            case 'testInput': // start splicing members until you run out, where you send error
-                if (args.length < 2) messageFunction('You must tag exactly two users to start game...');
-                else messageFunction('New game between ' + args[0] + ' and ' + args[1] + ' started!');
-                args = args.splice(2);
-                // shed spaces from both sides
-                // check if both @s are valid and instantiate game
-            break;
-
-            case 'crypto': // start splicing members until you run out, where you send error
-                if (args.length < 2) messageFunction('Format: !crypto base crypto');
-                else cryptoPrice(args[0], args[1]);
-                args = args.splice(2);
-            break;
-
-            default:
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Command not found! Type !help for more info'
-                });
-            break;
-         }
-     }
+      default:
+        bot.sendMessage({
+          to: channelID,
+          message: "Command not found! Type !help for more info",
+        });
+        break;
+    }
+  }
 });
 
-// Actual game stuff
-
-var currentGames = []
-
-
-function valid_player() {
-
-}
-
 function messageFunction(messageString) {
-    bot.sendMessage({
-        to: currChannelID,
-        message: messageString
-    });
+  bot.sendMessage({
+    to: currChannelID,
+    message: messageString,
+  });
 }
 
 // delete this, only added for fun --> potential betting feature?
 function cryptoPrice(base, crypto) {
-    price.getCryptoPrice(base, crypto).then(obj => { // Base for ex - USD, Crypto for ex - ETH 
-        messageFunction('Current price is: ' + obj.price)
-    }).catch(err => {
-        console.log(err)
+  price
+    .getCryptoPrice(base, crypto)
+    .then((obj) => {
+      // Base for ex - USD, Crypto for ex - ETH
+      messageFunction("Current price is: " + obj.price);
     })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function startInterval(_interval) {
+  // Store the id of the interval so we can clear it later
+  intervalId = setInterval(function () {
+    if (
+      message.guild.channels.cache.find((c) => c.name.toLowerCase() === "diary")
+    ) {
+      bot.sendMessage({
+        to: "diary",
+        message:
+          "Roses are red, violets are blue\nI'm dysfunctional, and so are you",
+      });
+    }
+  }, _interval);
 }
